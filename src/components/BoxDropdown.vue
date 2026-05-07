e16d4800689f623904cbb62d5d5a1f722f25fd71
mrIee <alexwlee.web@gmail.com>
2026-05-07 15:58:02 +1000

    Add random box for 'Always Random' pokemon

--- src/components/BoxDropdown.vue
+++ src/components/BoxDropdown.vue
@@ -1,15 +1,13 @@
 <template>
   <select class="tw:w-24 tw:mr-2 tw:rounded tw:border-2 tw:border-gray-800 tw:cursor-pointer" @change="handleOnChangeDropdown">
-    <option v-for="(index, key) in numBoxes" :key="key" :value="key" :selected="modelValue === index - 1">
-      {{ index === maxNumBoxes + 1 ? 'Random' : `Box ${index}` }}
+    <option v-for="(index, key) in maxNumBoxes" :key="key" :value="key" :selected="modelValue === index - 1">
+      {{ index === maxNumBoxes ? 'Random' : `Box ${index}` }}
     </option>
   </select>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import { mapState } from 'pinia';
-import { useSettingsStore } from '../store/settingsStore';
 import { MAX_NUM_BOXES } from '../util/constants';
 
 export default defineComponent({
@@ -26,14 +24,8 @@ export default defineComponent({
   emits: ['update:modelValue'],
   data() {
     return {
-      maxNumBoxes: MAX_NUM_BOXES,
+      maxNumBoxes: MAX_NUM_BOXES + 1,
     }
-  },
-  computed: {
-    ...mapState(useSettingsStore, ['alwaysRandom']),
-    numBoxes(): number {
-      return this.alwaysRandom ? MAX_NUM_BOXES + 1 : MAX_NUM_BOXES;
-    },
   },
   methods: {
     handleOnChangeDropdown(event: Event): void {
